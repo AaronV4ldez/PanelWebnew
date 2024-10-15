393aa46f99b103ed6ba53e3200a5b960cd998ad1
Aarón Valdez Garcia <valdezaaron04@gmail.com>
2024-10-15 11:49:18 -0600

    Chore: actualizacion de notas con visibilidad en otro link

--- PwebN/src/app/noticias-tabla-1/noticias-tabla-1.component.ts
+++ PwebN/src/app/noticias-tabla-1/noticias-tabla-1.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-noticias-tabla-1',
   templateUrl: './noticias-tabla-1.component.html',
   styleUrls: ['./noticias-tabla-1.component.css']
 })
-export class NoticiasTabla1Component {
+export class NoticiasTabla1Component implements OnInit {
+  noticias: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.cargarNoticias();
+  }
+
+  cargarNoticias(): void {
+    this.http.get<any[]>('https://apisprueba.fpfch.gob.mx/api/v1/mkt/news')
+      .subscribe(response => {
+        this.noticias = response;
+      }, error => {
+        console.error('Error al cargar las noticias', error);
+      });
+  }
 }
