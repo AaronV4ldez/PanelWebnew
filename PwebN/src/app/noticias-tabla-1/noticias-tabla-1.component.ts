ce21ecbb1c69c4dc5422076849c8d253211fc51a
Aarón Valdez Garcia <valdezaaron04@gmail.com>
2024-10-16 02:38:53 -0600

    chore: modal de noticias implementado

--- PwebN/src/app/noticias-tabla-1/noticias-tabla-1.component.ts
+++ PwebN/src/app/noticias-tabla-1/noticias-tabla-1.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Modal } from 'bootstrap'; 
 
 @Component({
   selector: 'app-noticias-tabla-1',
@@ -8,8 +10,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NoticiasTabla1Component implements OnInit {
   noticias: any[] = [];
+  noticiaSeleccionada: any = null;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     this.cargarNoticias();
@@ -22,5 +25,18 @@ export class NoticiasTabla1Component implements OnInit {
       }, error => {
         console.error('Error al cargar las noticias', error);
       });
+  }
+
+  abrirModal(noticia: any): void {
+    this.noticiaSeleccionada = noticia;
+    const modalElement = document.getElementById('noticiaModal');
+    if (modalElement) {
+      const modal = new Modal(modalElement);
+      modal.show();
+    }
+  }
+
+  safeUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
