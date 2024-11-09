7089e122a8217a83fe2c8fd7a48c60b681928a57
Aarón Valdez Garcia <valdezaaron04@gmail.com>
2024-11-09 11:59:43 -0600

    Fis: arreglar paginacion de tramites y acomodar el box de config de publicidad

--- PwebN/src/app/tramites/tramites.component.ts
+++ PwebN/src/app/tramites/tramites.component.ts
@@ -12,12 +12,14 @@ import * as bootstrap from 'bootstrap';
 })
 export class TramitesComponent implements OnInit {
   tramites: any[] = [];
-  tramitesOriginales: any[] = []; // Nueva propiedad para almacenar datos originales
+  tramitesOriginales: any[] = [];
   filtroForm: FormGroup;
   filtroTramiteModal: any;
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+  Math = Math; // Para usar Math en la plantilla
 
   constructor(private http: HttpClient, private fb: FormBuilder) {
-    // Inicializar el formulario de filtro
     this.filtroForm = this.fb.group({
       usuario: [''],
       tipoTramite: [''],
@@ -31,14 +33,12 @@ export class TramitesComponent implements OnInit {
     this.getTramites();
   }
 
-  // Obtener trámites desde la API
   getTramites(): void {
     const url = 'https://apisprueba.fpfch.gob.mx/api/v1/panel/procedures';
     const headers = { Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjg2OSwibmFtZSI6IkFhclx1MDBmM24gVmFsZGV6IEdhcmNpYSIsImV4cCI6MTcwNDkwMTM1N30.ORsWQWxVBCjlhItaZ1e63qBIqEL1LFOjKuydoEaDBZg' };
 
     this.http.get<any[]>(url, { headers }).subscribe(
       (data) => {
-        // Guardar los datos originales y mostrarlos en la tabla
         this.tramitesOriginales = data.map(tramite => ({
           folio: tramite.folio,
           nombreUsuario: tramite.user_full_name,
@@ -47,12 +47,17 @@ export class TramitesComponent implements OnInit {
           creadoEn: tramite.created_at,
           ultimoMovimiento: tramite.updated_at
         }));
-        this.tramites = [...this.tramitesOriginales]; // Copiar los datos originales para trabajar con el filtro
+        this.tramites = [...this.tramitesOriginales];
       },
       (error) => {
         console.error('Error al obtener los trámites', error);
       }
     );
+  }
+
+  // Cambia la página actual
+  cambiarPagina(pagina: number) {
+    this.currentPage = pagina;
   }
 
   // Exportar datos a Excel
@@ -103,7 +108,7 @@ export class TramitesComponent implements OnInit {
   // Limpiar los campos del formulario de filtro y restaurar los datos originales
   onLimpiar(): void {
     this.filtroForm.reset();
-    this.tramites = [...this.tramitesOriginales]; // Restaurar los datos originales
+    this.tramites = [...this.tramitesOriginales];
   }
 
   // Lógica para aplicar el filtro en los trámites
@@ -112,46 +117,40 @@ export class TramitesComponent implements OnInit {
       const coincideUsuario = filtroData.usuario
         ? tramite.nombreUsuario.toLowerCase().includes(filtroData.usuario.toLowerCase())
         : true;
-  
+
       const coincideTipoTramite = filtroData.tipoTramite
         ? this.normalizeString(tramite.eltramite) === this.normalizeString(filtroData.tipoTramite)
         : true;
-  
+
       const coincideEstatus = filtroData.estatus
         ? tramite.estatus === filtroData.estatus
         : true;
-  
-      // Filtrado por rango de fechas (solo si se selecciona alguna fecha)
+
       let coincideFecha = true;
       if (filtroData.fechaDesde || filtroData.fechaHasta) {
         const fechaDesde = filtroData.fechaDesde ? this.convertirFecha(filtroData.fechaDesde) : null;
         const fechaHasta = filtroData.fechaHasta ? this.convertirFecha(filtroData.fechaHasta) : null;
-  
-        // Convertir el valor de `ultimoMovimiento` usando la función personalizada
         const fechaUltimoMovimiento = this.convertirFecha(tramite.ultimoMovimiento);
-  
-        // Comparar fechas solo si `fechaDesde` o `fechaHasta` existen
         coincideFecha = (!fechaDesde || fechaUltimoMovimiento >= fechaDesde) &&
                         (!fechaHasta || fechaUltimoMovimiento <= fechaHasta);
       }
-  
+
       return coincideUsuario && coincideTipoTramite && coincideEstatus && coincideFecha;
     });
   }
-  
-  // Función para convertir la fecha correctamente
+
   convertirFecha(fechaStr: string): Date {
-    // Tratar de convertir una fecha con formato `YYYY-MM-DD HH:mm:ss` o `YYYY-MM-DD`
     const partes = fechaStr.split(' ');
-    const fechaParte = partes[0]; // `YYYY-MM-DD`
-    return new Date(fechaParte); // Crear un objeto `Date` solo con la fecha
+    const fechaParte = partes[0];
+    return new Date(fechaParte);
   }
-   
-  
 
-normalizeString(str: string): string {
-  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-}
+  normalizeString(str: string): string {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+  }
 
-
+  // Filtrado de trámites para la paginación
+  get filteredTramites() {
+    return this.tramites;
+  }
 }
